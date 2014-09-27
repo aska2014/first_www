59fd799208d8f6c54c9b91f30f555bdafc3fdd0c
Kareem Mohamed <kareem3d.a@gmail.com>
2014-09-27 23:21:51 +0200

    Fast commit

--- app/config/app.php
+++ app/config/app.php
@@ -121,8 +121,8 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
-        // 'Intervention\Image\ImageServiceProvider',
-        // 'Lifeentity\Images\ImagesServiceProvider'
+    'Intervention\Image\ImageServiceProvider',
+    'Lifeentity\Images\ImagesServiceProvider'
 	),
 
 	/*
