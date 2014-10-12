efe05a7c1b368d8c09a1d3a7ad8f13c03ccd6cf9
Kareem Mohamed <kareem3d.a@gmail.com>
2014-10-12 23:58:58 +0200

    Fast commit

--- app/views/template/footer.blade.php
+++ app/views/template/footer.blade.php
@@ -1,7 +1,8 @@
 <section class="twitter_feed_wrapper skincolored_section">
     <div class="container">
         <div class="row">
-            <div class="twitter_feed_icon wow fadeInDown"><a href="https://twitter.com/PlethoraThemes"><i
+
+            <div class="twitter_feed_icon wow fadeInDown"><a href="https://twitter.com/First_Choice_CO"><i
                         class="fa fa-twitter"></i></a></div>
             <div id="twitter_flexslider" class="flexslider">
                 <ul class="slides">
